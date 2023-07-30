8eca19c5d5d4dcd7feb44bf4329c21e9e59a50c8
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-07-30 14:50:37 -0300

    #git commit -m "Hello World"

---
+++ My First Project in C# - Hello World/My First Project in C# - Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Project_in_C____Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
