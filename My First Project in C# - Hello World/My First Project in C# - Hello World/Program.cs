210d19181b0de6b89bcaf54bd8a9a1b3676f87fe
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-07-31 07:59:29 -0300

    Variáveis  + Tipos

--- My First Project in C# - Hello World/My First Project in C# - Hello World/Program.cs
+++ My First Project in C# - Hello World/My First Project in C# - Hello World/Program.cs
@@ -10,10 +10,24 @@ namespace My_First_Project_in_C____Hello_World
     {
         static void Main(string[] args)
         {
+           //Maneira 1 de criar uma variável: usando o var
+           //Tipos de var iniciais : var,int,float(f no final),bool,string,char
+           //Maneira 2: Já indincado o tipo da variável
+            var boasvindas = "Olá, Seja bem vindo e feliz Páscoa";
+            int WW2 = 1942;
+            string corfav = "Amarelo";
+            float VF1 = 294.48f;
+            bool WW2_ocorreu = true;
+
             //\n ou Console.WriteLine pula uma linha
             //Console.Write escreve na tela
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(boasvindas);
             Console.WriteLine("Meu nome é João Victor!");
+            Console.WriteLine(WW2);
+            Console.WriteLine(corfav);
+            Console.WriteLine(VF1);
+            Console.WriteLine(WW2_ocorreu);
+
 
             //Console.ReadLine ele espera que o user digite algo ou clique em enter para continuar.
             Console.ReadLine();
