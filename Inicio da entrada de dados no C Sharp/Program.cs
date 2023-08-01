db9a3f337cd8729ebd2c01847fd2692c6e070c7b
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-08-01 08:01:10 -0300

    Primeiro exercicio com a entrada do usuário

---
+++ Inicio da entrada de dados no C Sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicio_da_entrada_de_dados_no_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é : ");
+            Console.WriteLine(nome);
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
