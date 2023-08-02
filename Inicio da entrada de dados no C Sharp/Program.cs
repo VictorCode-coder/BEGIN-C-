d08ffa654cbfa3287dc058b9ecf6b0ee0af2a8cd
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-08-02 08:02:50 -0300

    Operdores aritiméticos

--- Inicio da entrada de dados no C Sharp/Program.cs
+++ Inicio da entrada de dados no C Sharp/Program.cs
@@ -10,19 +10,28 @@ namespace Inicio_da_entrada_de_dados_no_C_Sharp
     {
         static void Main(string[] args)
         {
-            string nome = "";
+            int N1 = 5 + 5;
+            int N2 = 5 - 5;
+            int N3 = 5 * 5;
+            int N4 = 5 / 5;
 
-            Console.WriteLine("Escreva seu nome: ");
-            nome = Console.ReadLine();
-            Console.WriteLine("Seu nome é : ");
-            Console.WriteLine(nome);
-            
-            
-            
-            
-            
-            
-            
+            Console.WriteLine("A +, -, *, / entre os números 5 e 5 é : ");
+            Console.WriteLine(N1);
+            Console.WriteLine(N2);
+            Console.WriteLine(N3);
+            Console.WriteLine(N4);
+
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
