f606b4b38ec5170d90410e1f1581eeda938e45dc
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-08-16 08:43:29 -0300

    Primeiro exercício com Funções e Parâmetros

--- Inicio da entrada de dados no C Sharp/Program.cs
+++ Inicio da entrada de dados no C Sharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,35 +15,48 @@ namespace Inicio_da_entrada_de_dados_no_C_Sharp
             Console.WriteLine("------------------------------------");
             Console.WriteLine("         Analisador de idade");
             Console.WriteLine("------------------------------------");
-            Console.WriteLine("Digite o ano atual:");
-            int anoatual = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o ano em que você nasceu :");
-            int nascimento = Int32.Parse(Console.ReadLine());
-            int id= anoatual - nascimento;
-            Console.WriteLine("Sua idade é!");
-            Console.WriteLine(id);
-            if (id < 10)
-            {
-                Console.WriteLine("Você é uma criança, não pode dirigir!");
-            }
-            else if (id > 10 && id < 18)
-            {
-                Console.WriteLine("Você é um adolescente, não pode dirigir!");
-            }
-            else if (id >= 18 && id <= 25)
-            {
-                Console.WriteLine("Você já é maior de idade, pode ditigir!");
-            }
-            else if (id > 25 && id < 60)
-            {
-                Console.WriteLine("Você já é um adulto e pode dirigir!");
-            }
-            else if (id >= 60)   
-            {
-                Console.WriteLine("Você já é idoso, precisa descansar!");
-            }
-    
+            ano();
+
+
             Console.ReadLine();
+      
         }
-    }
+        static void ano()
+        {
+            Console.WriteLine("Digite o ano atual:");
+            int anoatual = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano em que você nasceu :");
+            int nascimento = int.Parse(Console.ReadLine());
+            idade(anoatual, nascimento);
+            
+        }
+
+        static void idade(int cal_id1, int cal_id2)
+        {
+            int ID = cal_id1 - cal_id2;
+            Console.WriteLine("Sua idade é:");
+            Console.WriteLine(ID);
+            fasevida(ID);
+        }
+        static void fasevida(int fasevida)
+        {
+            if (fasevida == 0)
+            {
+                Console.WriteLine("VOCÊ AINDA NÃO NASCEU!");
+            }
+            else if (fasevida > 0 && fasevida < 10)
+            {
+                Console.Write("Você é uma criança");
+            }
+            else if (fasevida > 10 && fasevida < 20)
+            {
+                Console.WriteLine("Você é um adolescente");
+            }
+            else if (fasevida >= 20)
+            {
+                Console.WriteLine("Você é um adulto");
+            }
+        }
+
+    }   
 }
