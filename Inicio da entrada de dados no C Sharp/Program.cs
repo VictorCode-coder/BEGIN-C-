c8ded0b26202536885a7df9be1289b0c96d00f71
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-09-07 09:53:59 -0300

    Prática de Arrays e retorno de Funções

--- Inicio da entrada de dados no C Sharp/Program.cs
+++ Inicio da entrada de dados no C Sharp/Program.cs
@@ -12,52 +12,44 @@ namespace Inicio_da_entrada_de_dados_no_C_Sharp
     {
         static void Main(string[] args)
         {
-            string nomeproduto = "";
-            string categoria = "";
-            string unidade = "";
-            float precofornecedor = 0; 
-            float precocusto = 0;
-            float lucrodesejado = 0;
+            string[] nomealuno = {"ANNA", "JOÃO", "PAULO","JULIA"};
+            float Prova1 = 0;
+            float Prova2 = 0;
+            float notasatividades = 0;
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("SEJA BEM VINDO AO DIÁRIO DE NOTAS DA ESCOLA");
+            Console.WriteLine("-------------------------------------------");
 
 
-
-            Console.WriteLine("-------------------------");
-            Console.WriteLine("   Gestão de Produtos");
-            Console.WriteLine("-------------------------");
-
-            Console.WriteLine("    Dados do Produto:");
-            Console.WriteLine("-------------------------");
-            Console.WriteLine("NOME DO PRODUTO :");
-            nomeproduto = Console.ReadLine();
-            Console.WriteLine("CATEGORIA DO PRODUTO :");
-            categoria = Console.ReadLine();
-            Console.WriteLine("UNIDADE DE MEDIDA DO PRODUTO :");
-            unidade = Console.ReadLine();
-            Console.WriteLine("PREÇO DE FORNECEDOR :");
-            precofornecedor = float.Parse(Console.ReadLine());
-            Console.WriteLine("PREÇO DE CUSTO :");
-            precocusto = float.Parse(Console.ReadLine());
-            Console.WriteLine("LUCRO DESEJADO(%) :");
-            lucrodesejado = float.Parse(Console.ReadLine());
-            Console.Clear();
-
-            precodoproduto(precocusto,lucrodesejado,nomeproduto,categoria,unidade);
-
-
-
+            Console.WriteLine(nomealuno[2]);
+            Console.WriteLine("NOTA DA PROVA 1:");
+            Prova1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("NOTA DA PROVA 2:");
+            Prova2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("NOTA DAS ATIVIDADES EM SALA");
+            notasatividades = float.Parse(Console.ReadLine());
+            
+            float valortotal = notafinal(Prova1, Prova2, notasatividades);
+            Console.WriteLine("A NOTA FINAL DO ALUNO É: " +  valortotal);   
 
             Console.ReadLine();
         }
-        static void precodoproduto(float preco, float lucro, string nome, string cat, string un)
-        {
-            float novopreco = ((preco * lucro) / 100) + preco;
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("NOME DO PRODUTO-----" + nome);
-            Console.WriteLine("CATEGORIA ---------------------" + cat);
-            Console.WriteLine("UNIDADE DE MEDIDA--------------" + un);
-        }   
+        static float notafinal(float P1, float P2, float NA)
+        {  
+            float total = P1 + P2 + NA;
+            return total;
 
 
+
+
+
+
+
+            
+        }
+
+        
 
     }   
 }
