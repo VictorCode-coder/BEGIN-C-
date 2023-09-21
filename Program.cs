f6579014c5910e853adfed0ddf06f9cad4d4f8be
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-09-21 08:48:10 -0300

    Nova forma de usar funções para a construção de melhores menus

--- Program.cs
+++ Program.cs
@@ -10,19 +10,18 @@ namespace Exercícios_de_Lógica_em_CSharp
     {
         static void Main(string[] args)
         {
+            Menu();     
+       
+        }
+
+        static void Menu()
+        {
             string menuopcoes = "";
-            float numero1 = 0.0f;
-            float numero2 = 0.0f;
-            float resultsoma = 0.0f;
-            float resultsub = 0.0f;
-            float resultmulti = 0.0f;
-             
 
             Console.WriteLine("--------------------------------------------------");
             Console.WriteLine("CALCULADORA - INCIANTES EM CSharp");
             Console.WriteLine("SELECIONE UMA DAS OPÇÕES A SEGUIR PARA CONTINUAR :");
             Console.WriteLine("--------------------------------------------------");
-
             Console.WriteLine("[1] - Somar");
             Console.WriteLine("[2] - Subtrair");
             Console.WriteLine("[3] - Multiplicador");
@@ -30,49 +29,11 @@ namespace Exercícios_de_Lógica_em_CSharp
             Console.WriteLine("[0] - Sair");
             menuopcoes = Console.ReadLine();
             Console.Clear();
-
-            switch (menuopcoes)
-            {
-                    case "1":
-                    Console.WriteLine("INFOMRE O 1º NÚMERO :");
-                    numero1 = float.Parse(Console.ReadLine());
-                    Console.WriteLine("INFORME O 2º NÚMERO :");
-                    numero2  = float.Parse(Console.ReadLine());
-                    resultsoma = numero1 + numero2;
-                    Console.WriteLine("O RESULTADO DA SOMA DESSE DOIS NÚMEROS É : " + resultsoma);
-                    //A partir daqui, o usuário deve ser re-direcionado para o menu principal
-                    break;
-                    case "2":
-                    Console.WriteLine("INFORME O 1º NÚMERO :");
-                    numero1 = float.Parse(Console.ReadLine());
-                    Console.WriteLine("INFORME O 2º NÚMERO :");
-                    numero2 = float.Parse(Console.ReadLine());
-                    resultsub = numero1 - numero2;
-                    Console.WriteLine("O RESULTADO DA SUBTRAÇÃO DESSES DOIS NÚMEROS É : " + resultsub);
-                    //A partir daqui, o usuário deve ser re-direcionado para o menu principal
-                        break;
-                    case "3":
-                    Console.WriteLine("INFORME O 1º NÚMERO :");
-                    numero1 = float.Parse(Console.ReadLine());
-                    Console.WriteLine("INFORME O 2º NÚMERO :");
-                    numero2 = float.Parse(Console.ReadLine());
-                    resultmulti = numero1 * numero2;
-                    Console.WriteLine("O RESULTADO DA MULTIPLICAÇÃO DESSES DOIS NÚMEROS É : " + resultmulti);
-                    //A partir daqui, o usuário deve ser re-direcionado para o menu principal
-                    break;
-                    case "4":
-                        break;
-                    case "5":
-                        break;
-
-            }
-
-
-
-
-
-
-            Console.ReadLine();
         }
+
+        //CRIAR UMA FUNÇÃO PARA CADA OPERAÇÃO. EM QUE NO FINAL ELA VAI CHAMAR A FUNÇÃO DO MENU NOVAMENTE
+        //// Desafios
+        // 1- Crie uma nova opção na calculadora (opção número 5 do menu) que retorne o resto da divisão.
+        // 2- Crie uma nova opção na calculadora (opção número 6 do menu) que retorne o resultado da potenciação de uma determinada base pelo seu expoente. Exemplo **2³ = 8**.
     }
 }
