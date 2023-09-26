aa2dbd839cf095d106e0f29e1d746061f405270f
João Victor Kalil <joaovictorkalil14@gmail.com>
2023-09-26 09:59:40 -0300

    Calculadora Versão Definitiva - faltando os desafios

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercícios_de_Lógica_em_CSharp
 {
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Menu();     
-       
+            Menu();
+
         }
 
         static void Menu()
@@ -29,11 +32,121 @@ namespace Exercícios_de_Lógica_em_CSharp
             Console.WriteLine("[0] - Sair");
             menuopcoes = Console.ReadLine();
             Console.Clear();
+
+            switch (menuopcoes)
+            {
+                case "1":
+                    Soma();
+                    break;
+
+                case "2":
+                    Subtraçao();
+                    break;
+
+                case "3":
+                    Multiplicacao();
+                    break;
+
+                case "4":
+                    Divisao();
+                    break;
+
+                default:
+                    Console.WriteLine("FECHANDO O PROGRAMA...ADEUS (; ;)");
+                    Console.ReadLine();
+                    break;
+
+
+            }
+
         }
+
+        static void Soma()
+        {
+            Console.WriteLine("DIGITE O 1º NÚMERO :");
+            float numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIGITE 0 2º NÚMERO :");
+            float numero2 = int.Parse(Console.ReadLine());
+            float results = numero1 + numero2;
+            Console.WriteLine(numero1 + " + " + numero2 + " = " + results);
+            Console.ReadLine();
+            Console.Clear();
+            Menu();
+        }
+        static void Subtraçao()
+        {
+            Console.WriteLine("DIGITE O 1º NÚMERO :");
+            float numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIGITE 0 2º NÚMERO :");
+            float numero2 = int.Parse(Console.ReadLine());
+            float results = numero1 - numero2;
+            Console.WriteLine(numero1 + " - " + numero2 + " = " + results);
+            Console.ReadLine();
+            Console.Clear();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("DIGITE O 1º NÚMERO :");
+            float numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIGITE 0 2º NÚMERO :");
+            float numero2 = int.Parse(Console.ReadLine());
+            float results = numero1 * numero2;
+            Console.WriteLine(numero1 + " x " + numero2 + " = " + results);
+            Console.ReadLine();
+            Console.Clear();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("DIGITE O 1º NÚMERO :");
+            float numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIGITE 0 2º NÚMERO :");
+            float numero2 = int.Parse(Console.ReadLine());
+            if (numero2 == 0)
+            {
+                Console.WriteLine("NÃO É POSSÍVEL DIVIDIR POR ZERO");
+                Console.ReadLine();
+                Console.Clear();
+                Divisao();
+            }
+            else
+            { 
+                float results = numero1 / numero2;
+                Console.WriteLine(numero1 + " / " + numero2 + " = " + results);
+                Console.ReadLine();
+                Console.Clear();
+                Menu();
+            }
+        }
+
+
 
         //CRIAR UMA FUNÇÃO PARA CADA OPERAÇÃO. EM QUE NO FINAL ELA VAI CHAMAR A FUNÇÃO DO MENU NOVAMENTE
         //// Desafios
         // 1- Crie uma nova opção na calculadora (opção número 5 do menu) que retorne o resto da divisão.
         // 2- Crie uma nova opção na calculadora (opção número 6 do menu) que retorne o resultado da potenciação de uma determinada base pelo seu expoente. Exemplo **2³ = 8**.
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+    
